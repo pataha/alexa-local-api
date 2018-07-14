af2e20e3336a17c28f5b4356ec92257eb9a3d8c8
Hai Phan <pataha@users.noreply.github.com>
2018-07-14 16:27:33 +0700

    remove ENV

--- alexa-api/run.sh
+++ alexa-api/run.sh
@@ -4,7 +4,7 @@ set -e
 WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
 
-export USER=$(jq --raw-output ".username" $CONFIG_PATH); export PASS=$(jq --raw-output ".password" $CONFIG_PATH); export URL=$(jq --raw-output ".url" $CONFIG_PATH)
+# export USER=$(jq --raw-output ".username" $CONFIG_PATH); export PASS=$(jq --raw-output ".password" $CONFIG_PATH); export URL=$(jq --raw-output ".url" $CONFIG_PATH)
 
 Xvfb -ac -screen scrn 1280x2000x24 :99.0 &
 export DISPLAY=:99.0
