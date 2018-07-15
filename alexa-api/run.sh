5020c5a81d0992b3b8a3b5992b7d4ee3e0143cc0
Hai Phan <hairbeos@gmail.com>
2018-07-15 16:10:21 +0700

    update Xvfb config

--- alexa-api/run.sh
+++ alexa-api/run.sh
@@ -6,7 +6,7 @@ CONFIG_PATH=/data/options.json
 
 # export USER=$(jq --raw-output ".username" $CONFIG_PATH); export PASS=$(jq --raw-output ".password" $CONFIG_PATH); export URL=$(jq --raw-output ".url" $CONFIG_PATH)
 
-Xvfb -ac -screen scrn 1280x2000x24 :99.0 &
-export DISPLAY=:99.0
+Xvfb -ac -screen scrn 1280x2000x24 :9.0 &
+export DISPLAY=:9.0
 
 DEBUG=nightmare* node index.js
